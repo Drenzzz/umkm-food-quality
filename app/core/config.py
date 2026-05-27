76b7974b63ba26a3b9dbdb1409e09945ef741d4b
Drenzzz <realdrenzzz@gmail.com>
2026-05-27 20:49:17 +0700

    fix(cors): allow https localhost origin

--- app/core/config.py
+++ app/core/config.py
@@ -6,6 +6,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 DEFAULT_CORS_ORIGINS = [
     "http://localhost",
+    "https://localhost",
     "capacitor://localhost",
     "ionic://localhost",
 ]
